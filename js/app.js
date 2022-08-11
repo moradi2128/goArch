393fb50a702593bee16aeeafafa0f5046f65a8ec
mohamadreza  moradi <moradi2565@gmail.com>
2022-08-11 17:18:53 +0430

    make changes

--- js/app.js
+++ js/app.js
@@ -1,57 +1,65 @@
+new WOW().init();
+
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 2,
-  spaceBetween: 0,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-      
+    slidesPerView: 2,
+    spaceBetween: 0,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
     },
-    768: {
-      slidesPerView: 2,
-      
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-    1024: {
-      slidesPerView:3,
-      
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
     },
-  },
 });
 
-new SmoothScroll('a[href*="#"]', {
-  easing: "linear",
-});
-
-$(window).on("scroll load", function () {
-  if (
-    $("#about .container").offset().top - $(window).scrollTop() <
-    $(window).height() - 200
-  ) {
-    $("#about .container").addClass("slideInRight").removeClass("invisible");
-  }
-
-  $("#authors .card").each(function () {
-    if (
-      $(this).offset().top - $(window).scrollTop() <
-      $(window).height() - 200
-    ) {
-      $(this).addClass("zoomIn").removeClass("invisible");
+// === NanBar ===
+$(window).on("scroll load", function() {
+    if ($(window).scrollTop() > $(window).height()) {
+        $(".navbar").addClass("navbar-glass");
+        $(".navbar-container").addClass("menu-container");
+        $(".logo-glass").css("display", "block");
+        $(".backToTop").css("visibility", "visible");
+    } else {
+        $(".navbar").removeClass("navbar-glass ");
+        $(".navbar-container").removeClass("menu-container");
+        $(".logo-glass").css("display", "none");
+        $(".backToTop").css("visibility", "hidden");
     }
-  });
+});
 
-  if (
-    $("#contact form").offset().top - $(window).scrollTop() <
-    $(window).height() - 200
-  ) {
-    $("#contact .col-lg-9").addClass("slideInRight").removeClass("invisible");
-    $("#contact .col-lg-3").addClass("slideInUp").removeClass("invisible");
-  }
+// === Back To Top ===
+$(window).scroll(function() {
+    if ($(this).scrollTop()) {
+        $(".backToTop").fadeIn();
+    } else {
+        $(".backToTop").fadeOut();
+    }
+});
+$(".backToTop").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 700);
+});
+
+$(".nav-buttom-res").click(function(e) {
+    var menuItem = $(this);
+
+    if (menuItem.attr("aria-expanded") === "true") {
+        $(".carousel-indicators").css("z-index", 10001);
+        console.log("true");
+    } else if (menuItem.attr("aria-expanded") === "false") {
+        $(".carousel-indicators").css("z-index", 1);
+        console.log("false");
+    }
 });
